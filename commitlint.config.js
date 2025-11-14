d2132d376ce52f9af052ebb28c23d0d255f34cec
felixschildorfer <32631391+felixschildorfer@users.noreply.github.com>
2025-11-14 11:24:31 -0800

    chore: set up commitlint, husky, and github actions CI

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',      // A new feature
+        'fix',       // A bug fix
+        'docs',      // Documentation only changes
+        'style',     // Changes that don't affect code meaning (formatting, etc)
+        'refactor',  // Code change that neither fixes a bug nor adds a feature
+        'perf',      // Code change that improves performance
+        'test',      // Adding missing tests or correcting existing tests
+        'chore',     // Changes to build process, dependencies, tooling
+        'ci',        // Changes to CI configuration files and scripts
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'header-max-length': [2, 'always', 100],
+  },
+};
